b5d376e546903d26b7489d5843909e110dbb6c25
lsbwiki <lingdian@live.com>
2026-02-22 01:05:41 +0900

    Update and rename start.sh to install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ echo "------------------------------------------------"
 wget -qO- https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -
 
 echo "------------------------------------------------"
-echo "测速完成，正在以写死配置启动 Xray..."
+echo "测速完成，启动 Xray (UUID: 535a6590...)"
 echo "------------------------------------------------"
 
-# 直接启动，不再使用 sed 替换
+# 启动服务
 exec xray -config /etc/xray/config.json
